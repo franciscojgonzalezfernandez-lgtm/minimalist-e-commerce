55125072cdc8a3709d899220ec52f4ae988e36d5
Francisco Javier González <franciscojgonzalezfernandez@gmail.com>
2025-12-03 14:25:41 +0100

    if the id is 'new', return an empty product to not go out of new product page

--- src/actions/get-product.actions.ts
+++ src/actions/get-product.actions.ts
@@ -2,11 +2,25 @@ import { teslaApi } from "@/api/TesloApi";
 import type { Product } from "@/interfaces/Product";
 import { ROUTE_TO_IMAGES } from "./get-products.actions";
 
+const DUMMY_PRODUCT: Product = {
+  id: "",
+  title: "",
+  price: 0,
+  description: "",
+  slug: "",
+  stock: 0,
+  sizes: [],
+  gender: undefined,
+  tags: [],
+  images: [],
+};
+
 interface Props {
   idSlug: string;
 }
 
 export const getProductAction = async ({ idSlug }: Props): Promise<Product> => {
+  if (idSlug === "new") return DUMMY_PRODUCT;
   const { data } = await teslaApi.get<Product>(`/products/${idSlug}`);
 
   // image mapping
