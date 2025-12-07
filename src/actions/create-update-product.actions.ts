90b19461790658e30e7f276c4bf7d52318f03558
Francisco Javier González <franciscojgonzalezfernandez@gmail.com>
2025-12-07 15:41:15 +0100

    Action to update or create a new product ready

---
+++ src/actions/create-update-product.actions.ts
@@ -0,0 +1,27 @@
+import { teslaApi } from "@/api/TesloApi";
+import type { Product } from "@/interfaces/Product";
+
+export const createUpdateProductaction = async (
+  productLike: Partial<Product>
+): Promise<Product> => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { id, user, images = [], ...restProps } = productLike;
+
+  const isCreating = id === "new";
+
+  restProps.price = Number(restProps.price || 0);
+  restProps.stock = Number(restProps.stock || 0);
+
+  const { data } = await teslaApi<Product>({
+    method: isCreating ? "POST" : "PATCH",
+    url: isCreating ? "/products" : `/products/${id}`,
+  });
+
+  return {
+    ...data,
+    images: images.map((image) => {
+      if (image.includes("http")) return image;
+      return `${import.meta.env.VITE_API_URL}/files/product/${image}`;
+    }),
+  };
+};
